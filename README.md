5741b68d67f4821c17382e58dc7988a0203630dc
ROM <cp.web@yandex.ru>
2018-11-22 10:26:25 +0300

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+Webp "new" image format from 2010 by Google, but not so easy with compability
+
+jpg 87kb --> 11kb webp
+
+```
+npm i
+node app.js
+
+```
---
+++ app.js
@@ -0,0 +1,8 @@
+const imagemin = require("imagemin");
+const imageminWebp = require("imagemin-webp");
+
+imagemin(["images/*.{jpg,png}"], "build/images", {
+  use: [imageminWebp({ quality: 50 })]
+}).then(() => {
+  console.log("Images optimized");
+});
